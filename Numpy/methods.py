79d1af8b541dfba011e116b74ac843b12b1e7e82
DarshanKaladiya <kaladiyadarshan18@gmail.com>
2026-09-22 08:06:04 +0530

    Numpy methods

---
+++ Numpy/methods.py
@@ -0,0 +1,22 @@
+#method 1
+
+import numpy
+
+a = numpy.array([1, 2, 3, 4, 5])
+print(a)
+
+print("=========================== \n")
+
+#method 2
+
+import numpy as nm
+b = nm.array([10.1, 20.2, 30.3, 40.4])
+print(b)
+
+print("=========================== \n")
+
+#method 3
+
+from numpy import *
+c = array(['a', 'b', 'c', 'd'])
+print(c)
